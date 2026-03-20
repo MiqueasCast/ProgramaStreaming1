ebb69c3fe11fb361cd2cfbe09be663824a123177
MiqueasCast <castmicah@gmail.com>
2026-03-19 18:52:38 -0600

    Sistema de validacion de entradas integrada

--- ProgramaStreaming/Program.cs
+++ ProgramaStreaming/Program.cs
@@ -17,7 +17,79 @@ do
         {
             case 1:
                 Console.Clear();
-                Console.WriteLine("opcion 1");
+                int tipoContenido;
+                bool entradaValida = false;
+
+                while (!entradaValida)
+                {
+                    Console.Write("Tipo de contenido: \n1. Pelicula\n2. Serie\n3. Documental\n4. Evento en vivo\n> ");
+                    entradaValida = int.TryParse(Console.ReadLine(), out tipoContenido) && tipoContenido >= 1 && tipoContenido <= 4;
+
+                    if (!entradaValida)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Opción inválida. Elige entre 1 y 4.\n");
+                    }
+                }
+                double duracionMinutos;
+                bool entradaValida2 = false;
+
+                while (!entradaValida2)
+                {
+                    Console.Write("Duracion en minutos: \n> ");
+                    entradaValida2 = double.TryParse(Console.ReadLine(), out duracionMinutos) && duracionMinutos > 0 && duracionMinutos < 500;
+
+                    if (!entradaValida2)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Ingrese una cifra en minutos valida\n");
+                    }
+                }
+                int clasificacion;
+                bool entradaValida3 = false;
+
+                while (!entradaValida3)
+                {
+                    Console.Write("Clasificacion: \n1. Todo publico\n2. +13\n3. +18\n> ");
+                    entradaValida3 = int.TryParse(Console.ReadLine(), out clasificacion) && clasificacion >= 1 && clasificacion <= 3;
+
+                    if (!entradaValida3)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Opción inválida. Elige entre 1 y 3.\n");
+                    }
+
+                }
+                int horaProgramada;
+                bool entradaValida4 = false;
+          
+                while (!entradaValida4)
+                {
+                    Console.Write("Hora programda: (0-23)\n> ");
+                    entradaValida4 = int.TryParse(Console.ReadLine(), out horaProgramada) && horaProgramada >0 && horaProgramada <= 23;
+
+                    if (!entradaValida4)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Opción inválida. Elija un horario valido.\n");
+                    }
+                }
+                int nivelProduccion;
+                bool entradaValida5 = false;
+
+                while (!entradaValida5)
+                {
+                    Console.Write("Nivel de producción:\n1. Bajo\n2. Medio\n3. Alto\n> ");
+                    entradaValida5 = int.TryParse(Console.ReadLine(), out nivelProduccion) && nivelProduccion >= 1 && nivelProduccion <= 3;
+
+                    if (!entradaValida5)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Opción inválida. Elija una opción valida\n");
+                    }
+                }
+                Console.Clear();
+
                 break;
             case 2:
 
